3bec3cf543232239299ae2ae57bd485436efef86
Tsudhonihm <your.email@example.com>
2025-03-13 03:43:22 -0400

    I hope this works

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Flask backend URL
+        target: 'http://localhost:5000', // Flask backend URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
